c03018ab541c08a7db090abbe468467491721ac8
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 00:53:53 +0300

    Add files via upload

---
+++ 2.10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            if (k >= 10)
+            {
+                Console.WriteLine("1. Число десятков = " + k / 10); 
+                Console.WriteLine("2. Число единиц = {0}", k % 10);
+                Console.WriteLine("3. Сумма его цифр = {0}", k % 10 + k / 10);
+                Console.WriteLine("4. Произведение его цифр = " + ((k % 10) * (k / 10)));
+            }
+            else
+            {
+                Console.WriteLine("Введите число от 10!");
+            }
+        }
+    }
+
+}
